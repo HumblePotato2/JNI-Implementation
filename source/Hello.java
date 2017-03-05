7d0682555b07025a17b0a47960a0e83110b86f8e
Humble Potato II <humblepotato2@gmail.com>
2017-03-06 05:15:34 +0800

    Java source file

--- source/Hello.java
+++ source/Hello.java
@@ -7,7 +7,15 @@
 public class Hello {
 	
 	/** Loads the native C/C++ library program. */
-	static { System.loadLibrary("libhello"); }
+	static {
+		System.loadLibrary("libhello");
+		// Linux native libraries should start with the lib prefix followed
+		// by the library name. This is important when loading Linux Shared
+		// Object (SO) libraries so in that case, the above code should be
+		// System.loadLibrary("hello"); where hello is the soname - the name
+		// of the library. More details at:
+		// http://tldp.org/HOWTO/Program-Library-HOWTO/shared-libraries.html
+	}
 	
 	/** The library's {@code void} type function. */
 	public native void print();
